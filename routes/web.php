31049c80721cfd98c1c892b98fc20f46e910d027
Poowanai.S <poowanai.saithai@gmail.com>
2025-02-22 21:56:56 +0700

    fix: add routing route-clear and refactor

--- routes/web.php
+++ routes/web.php
@@ -16,53 +16,50 @@ Route::get('/', function () {
 })->name('welcome');
 
 Route::get('/users/dashboard', function () {
-     return view('users.dashboard');
- })->middleware(['auth', 'verified','user'])->name('users.dashboard');
-
- 
- Route::post('/upload-image', [ImageUploadController::class, 'store']);
+    return view('users.dashboard');
+})->middleware(['auth', 'verified', 'user'])->name('users.dashboard');
 
 
- Route::get('/supervisor/dashboard', function () {
-     return view('supervisor.dashboard');
- })->middleware(['auth', 'verified','supervisor'])->name('supervisor.dashboard');
-
- Route::get('/admin/dashboard', function () {
-     return view('admin.dashboard');
- })->middleware(['auth', 'verified','admin'])->name('admin.dashboard');
+Route::post('/upload-image', [ImageUploadController::class, 'store']);
 
 
+Route::get('/supervisor/dashboard', function () {
+    return view('supervisor.dashboard');
+})->middleware(['auth', 'verified', 'supervisor'])->name('supervisor.dashboard');
 
-  Route::get('/dashboard', function () {
-      return view('dashboard');
-  })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('admin.dashboard');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-     
- });
+});
 
-Route::get('logout', function(){
+Route::get('logout', function () {
     auth()->logout();
-
     return to_route('login');
 });
 
 Route::get('/installments/create', [InstallmentController::class, 'create'])
-->middleware(['auth', 'admin'])  // Ensure user is authenticated and an admin
-->name('installments.create');
+    ->middleware(['auth', 'admin'])  // Ensure user is authenticated and an admin
+    ->name('installments.create');
 
 Route::post('/installments', [InstallmentController::class, 'store'])
     ->middleware(['auth', 'admin'])  // Ensure only authenticated and admin users can access
     ->name('installments.store');
 
-    Route::get('/print-receipt', [InstallmentController::class, 'printReceipt'])->name('print.receipt');
-   
+Route::get('/print-receipt', [InstallmentController::class, 'printReceipt'])->name('print.receipt');
 
-    Route::get('/layouts/registeradmin', [RegisterController::class, 'showRegistrationForm'])
+
+Route::get('/layouts/registeradmin', [RegisterController::class, 'showRegistrationForm'])
     ->middleware(['auth', 'admin'])  // Ensure user is authenticated and an admin
     ->name('layouts.registeradmin');
 
@@ -70,7 +67,7 @@ Route::post('/registeradmin', [RegisterController::class, 'store'])
     ->middleware(['auth', 'admin'])  // Ensure user is authenticated and an admin
     ->name('registeradmin.store');
 
-    // Route for editing user details
+// Route for editing user details
 Route::get('/users/{user}/edit', [RegisterController::class, 'edit'])->name('users.edit')->middleware('auth', 'admin');
 
 // Route to update user details
@@ -80,24 +77,29 @@ Route::put('/users/{user}', [RegisterController::class, 'update'])->name('users.
 Route::delete('/users/{user}', [RegisterController::class, 'destroy'])->name('users.destroy')->middleware('auth', 'admin');
 
 Route::get('/users', [RegisterController::class, 'showUsers'])
-    ->middleware(['auth', 'admin'])  
+    ->middleware(['auth', 'admin'])
     ->name('users.list');
 
 
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 
 
-    Route::middleware('auth')->group(function () {
-        // Route for /dashboard
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    
-        Route::get('users/dashboard', [DashboardController::class, 'index'])->name('users.dashboard');
-    });
+Route::middleware('auth')->group(function () {
+    // Route for /dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/installments/pdf/{id}', [PDFController::class, 'generatePDF'])
-            ->name('installments.pdf')
-            ->middleware('verified'); // กรณีต้องการให้ยืนยันอีเมลก่อนใช้
-    });
+    Route::get('users/dashboard', [DashboardController::class, 'index'])->name('users.dashboard');
+});
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    Route::get('/installments/pdf/{id}', [PDFController::class, 'generatePDF'])
+        ->name('installments.pdf')
+        ->middleware('verified'); // กรณีต้องการให้ยืนยันอีเมลก่อนใช้
+});
+
+Route::get('/route-clear', function () {
+    Artisan::call('route:clear');
+    return "Routes cache cleared!";
+});
+
+require __DIR__ . '/auth.php';
